7c25ca3d92be9cc5bddd5b36baf29d941b47a873
takadr <t4k4.cr@gmail.com>
2023-08-23 18:56:04 +0900

    Added deploy scripts for Distributor and YMWK

---
+++ deploy/005_distributor.ts
@@ -0,0 +1,20 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { deploy, getFoundation, getContractAddress } from "../src/deployUtil";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { ethers } = hre;
+  const { getContractFactory } = ethers;
+  const foundation = await getFoundation();
+  const factoryAddress = getContractAddress(hre.network.name, "Factory");
+  const ymwkAddress = getContractAddress(hre.network.name, "YMWK");
+
+  await deploy("Distributor", {
+    from: foundation,
+    args: [factoryAddress, ymwkAddress],
+    log: true,
+    getContractFactory,
+  });
+};
+export default func;
+func.tags = ["Distributor"];
