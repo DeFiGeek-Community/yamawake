b99853f0ac8463d91d7ed376e9d747c211eb7c8b
takadr <t4k4.cr@gmail.com>
2024-10-29 11:30:38 +0900

    migrate integration tests for VotingEscrow from ethers v5 to v6

--- test/v1.5/integration/VotingEscrow/depositWithdrawVoting.test.ts
+++ test/v1.5/integration/VotingEscrow/depositWithdrawVoting.test.ts
@@ -1,33 +1,31 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { BigNumber, Contract } from "ethers";
 import {
   time,
   takeSnapshot,
   SnapshotRestorer,
 } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { MockToken, VotingEscrow } from "../../../../typechain-types";
 
 const ACCOUNT_NUM = 10;
 const MAX_EXAMPLES = 30; // テストの試行回数
 const STATEFUL_STEP_COUNT = 20; // ruleの実行回数
-const YEAR = BigNumber.from(86400 * 365);
-const WEEK = BigNumber.from(86400 * 7);
-const ten_to_the_40 = BigNumber.from(
-  "10000000000000000000000000000000000000000"
-);
+const YEAR = BigInt(86400 * 365);
+const WEEK = BigInt(86400 * 7);
+const ten_to_the_40 = BigInt("10000000000000000000000000000000000000000");
 
 describe("VotingEscrow", function () {
   let accounts: SignerWithAddress[];
-  let votingEscrow: Contract;
-  let token: Contract;
+  let votingEscrow: VotingEscrow;
+  let token: MockToken;
 
   let stAccountN: number;
   let stAccount: SignerWithAddress;
-  let stValue: BigNumber = BigNumber.from("0");
-  let stLockDuration: BigNumber;
-  let votingBalances: { [key: string]: BigNumber }[];
-  let unlockTime: BigNumber;
+  let stValue: bigint = 0n;
+  let stLockDuration: bigint;
+  let votingBalances: { [key: string]: bigint }[];
+  let unlockTime: bigint;
 
   let snapshot: SnapshotRestorer;
 
@@ -42,7 +40,7 @@ describe("VotingEscrow", function () {
     await token.waitForDeployment();
 
     votingEscrow = await VotingEscrow.deploy(
-      token.address,
+      token.target,
       "Voting-escrowed token",
       "vetoken",
       "v1"
@@ -56,13 +54,13 @@ describe("VotingEscrow", function () {
         ._mintForTesting(accounts[i].address, ten_to_the_40);
       await token
         .connect(accounts[i])
-        .approve(votingEscrow.address, ethers.constants.MaxUint256);
+        .approve(votingEscrow.target, ethers.MaxUint256);
     }
 
     //setup
     votingBalances = new Array(ACCOUNT_NUM).fill({
-      value: BigNumber.from("0"),
-      unlockTime: BigNumber.from("0"),
+      value: 0n,
+      unlockTime: 0n,
     });
   });
 
@@ -72,8 +70,8 @@ describe("VotingEscrow", function () {
 
   //--------------------------------------------- functions -----------------------------------------------------------//
 
-  function rdmValue(a: number): BigNumber {
-    let rdm = BigNumber.from(Math.floor(Math.random() * a).toString());
+  function rdmValue(a: number): bigint {
+    let rdm = BigInt(Math.floor(Math.random() * a).toString());
     return rdm;
   }
 
@@ -92,22 +90,22 @@ describe("VotingEscrow", function () {
     //number of weeks to lock a deposit
     stLockDuration = rdmValue(255); //uint8.max
 
-    let timestamp = BigNumber.from(await time.latest());
-    unlockTime = timestamp.add(WEEK.mul(stLockDuration)).div(WEEK).mul(WEEK);
+    let timestamp = BigInt(await time.latest());
+    unlockTime = ((timestamp + WEEK * stLockDuration) / WEEK) * WEEK;
 
-    if (stValue.eq(0)) {
+    if (stValue === 0n) {
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("need non-zero value");
-    } else if (votingBalances[stAccountN]["value"].gt("0")) {
+    } else if (votingBalances[stAccountN]["value"] > 0) {
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Withdraw old tokens first");
-    } else if (unlockTime.lte(timestamp)) {
+    } else if (unlockTime <= timestamp) {
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Can only lock until time in the future");
-    } else if (unlockTime.gte(timestamp.add(YEAR.mul("4")))) {
+    } else if (unlockTime >= timestamp + YEAR * 4n) {
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Voting lock can be 4 years max");
@@ -116,10 +114,23 @@ describe("VotingEscrow", function () {
         .connect(stAccount)
         .createLock(stValue, unlockTime);
       const receipt = await tx.wait();
-      votingBalances[stAccountN] = {
-        value: stValue,
-        unlockTime: receipt.events[1]["args"]["locktime"],
-      };
+      const VotingEscrow = await ethers.getContractFactory("VotingEscrow");
+      const iContract = VotingEscrow.interface;
+      for (const log of receipt!.logs) {
+        try {
+          const parsedLog = iContract.parseLog(log);
+          if (parsedLog?.name == `Deposit`) {
+            const [, , locktime] = parsedLog?.args;
+            votingBalances[stAccountN] = {
+              value: stValue,
+              unlockTime: locktime,
+            };
+            break;
+          }
+        } catch (error) {
+          return "";
+        }
+      }
     }
   }
 
@@ -134,24 +145,24 @@ describe("VotingEscrow", function () {
     //stValue
     stValue = rdmValue(9007199254740991);
 
-    let timestamp = BigNumber.from(await time.latest());
+    let timestamp = BigInt(await time.latest());
 
-    if (stValue.eq(0)) {
+    if (stValue === 0n) {
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("dev: need non-zero value");
-    } else if (votingBalances[stAccountN]["value"].eq("0")) {
+    } else if (votingBalances[stAccountN]["value"] === 0n) {
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("No existing lock found");
-    } else if (votingBalances[stAccountN]["unlockTime"].lte(timestamp)) {
+    } else if (votingBalances[stAccountN]["unlockTime"] <= timestamp) {
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("Cannot add to expired lock. Withdraw");
     } else {
       await votingEscrow.connect(stAccount).increaseAmount(stValue);
       votingBalances[stAccountN]["value"] =
-        votingBalances[stAccountN]["value"].add(stValue);
+        votingBalances[stAccountN]["value"] + stValue;
     }
   }
 
@@ -164,26 +175,23 @@ describe("VotingEscrow", function () {
     stAccount = accounts[stAccountN];
 
     //unlockTime
-    let timestamp = BigNumber.from(await time.latest());
+    let timestamp = BigInt(await time.latest());
     stLockDuration = rdmValue(255); //number of weeks
-    let unlockTime = timestamp
-      .add(stLockDuration.mul(WEEK))
-      .div(WEEK)
-      .mul(WEEK);
+    let unlockTime = ((timestamp + stLockDuration * WEEK) / WEEK) * WEEK;
 
-    if (votingBalances[stAccountN]["unlockTime"].lte(timestamp)) {
+    if (votingBalances[stAccountN]["unlockTime"] <= timestamp) {
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Lock expired");
-    } else if (votingBalances[stAccountN]["value"].eq("0")) {
+    } else if (votingBalances[stAccountN]["value"] === 0n) {
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Nothing is locked");
-    } else if (votingBalances[stAccountN]["unlockTime"].gte(unlockTime)) {
+    } else if (votingBalances[stAccountN]["unlockTime"] >= unlockTime) {
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Can only increase lock duration");
-    } else if (unlockTime.gt(timestamp.add(YEAR.mul("4")))) {
+    } else if (unlockTime > timestamp + YEAR * 4n) {
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Voting lock can be 4 years max");
@@ -192,8 +200,20 @@ describe("VotingEscrow", function () {
         .connect(stAccount)
         .increaseUnlockTime(unlockTime);
       const receipt = await tx.wait();
-      votingBalances[stAccountN]["unlockTime"] =
-        receipt.events[0]["args"]["locktime"];
+      const VotingEscrow = await ethers.getContractFactory("VotingEscrow");
+      const iContract = VotingEscrow.interface;
+      for (const log of receipt!.logs) {
+        try {
+          const parsedLog = iContract.parseLog(log);
+          if (parsedLog?.name == `Deposit`) {
+            const [, , locktime] = parsedLog?.args!;
+            votingBalances[stAccountN]["unlockTime"] = locktime;
+            break;
+          }
+        } catch (error) {
+          return "";
+        }
+      }
     }
   }
 
@@ -206,16 +226,16 @@ describe("VotingEscrow", function () {
     stAccountN = rdm;
     stAccount = accounts[stAccountN];
 
-    let timestamp = BigNumber.from(await time.latest());
+    let timestamp = BigInt(await time.latest());
 
-    if (votingBalances[stAccountN]["unlockTime"].gt(timestamp)) {
+    if (votingBalances[stAccountN]["unlockTime"] > timestamp) {
       // console.log("--reverted");
       await expect(votingEscrow.connect(stAccount).withdraw()).to.revertedWith(
         "The lock didn't expire"
       );
     } else {
       await votingEscrow.connect(stAccount).withdraw();
-      votingBalances[stAccountN]["value"] = BigNumber.from("0");
+      votingBalances[stAccountN]["value"] = 0n;
     }
   }
 
@@ -233,20 +253,20 @@ describe("VotingEscrow", function () {
   async function ruleAdvanceTime() {
     console.log("ruleAdvanceTime");
 
-    let stSleepDuration = Math.floor(Math.random() * 3) + 1; //1~4
+    let stSleepDuration = BigInt(Math.floor(Math.random() * 3) + 1); //1~4
 
-    await time.increase(WEEK.mul(stSleepDuration).toNumber());
+    await time.increase(WEEK * stSleepDuration);
   }
 
   async function checkInvariants() {
     for (let i = 0; i < ACCOUNT_NUM; i++) {
       expect(await token.balanceOf(accounts[i].address)).to.equal(
-        ten_to_the_40.sub(votingBalances[i]["value"])
+        ten_to_the_40 - votingBalances[i]["value"]
       );
     }
 
-    let total_supply = BigNumber.from("0");
-    let timestamp = BigNumber.from(await time.latest());
+    let total_supply = 0n;
+    let timestamp = BigInt(await time.latest());
 
     for (let i = 0; i < ACCOUNT_NUM; i++) {
       let data = votingBalances[i];
@@ -254,23 +274,23 @@ describe("VotingEscrow", function () {
       let balance = await votingEscrow["balanceOf(address)"](
         accounts[i].address
       );
-      total_supply = total_supply.add(balance);
+      total_supply = total_supply + balance;
 
-      if (data["unlockTime"].gt(timestamp) && data["value"].div(YEAR).gt("0")) {
-        expect(balance.isZero()).to.equal(false);
-      } else if (data["value"].isZero() || data["unlockTime"].lte(timestamp)) {
-        expect(balance.isZero()).to.equal(true);
+      if (data["unlockTime"] > timestamp && data["value"] / YEAR > 0) {
+        expect(balance === 0n).to.equal(false);
+      } else if (data["value"] === 0n || data["unlockTime"] >= timestamp) {
+        expect(balance === 0n).to.equal(true);
       }
     }
     expect(await votingEscrow["totalSupply()"]()).to.equal(total_supply);
 
-    total_supply = BigNumber.from("0");
+    total_supply = 0n;
     let blocknumber = (await time.latestBlock()) - 4;
 
     for (let i = 0; i < ACCOUNT_NUM; i++) {
-      total_supply = total_supply.add(
-        await votingEscrow.balanceOfAt(accounts[i].address, blocknumber)
-      );
+      total_supply =
+        total_supply +
+        (await votingEscrow.balanceOfAt(accounts[i].address, blocknumber));
     }
 
     expect(await votingEscrow.totalSupplyAt(blocknumber)).to.equal(
@@ -292,7 +312,7 @@ describe("VotingEscrow", function () {
     it(`Try ${x}`, async () => {
       let steps = Math.floor(Math.random() * (STATEFUL_STEP_COUNT - 1)) + 1;
       for (let i = 0; i < steps; i++) {
-        let n = (await rdmValue(func.length)).toNumber();
+        let n = Number(rdmValue(func.length));
         await func[n]();
         await checkInvariants();
       }
